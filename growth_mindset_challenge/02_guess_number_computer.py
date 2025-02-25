640fab7a744b1cda5824ddc9dd174f03bccb4fc3
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-02-25 15:13:30 +0500

    updated

--- growth_mindset_challenge/02_guess_number_computer.py
+++ growth_mindset_challenge/02_guess_number_computer.py
@@ -2,28 +2,36 @@ import random
 
 
 def play_game():
-    number_to_guess = random.randrange(1, 100)
+    # Range of Selection
+    number_to_guess = random.randrange(1, 10)
+    # Number of Attempts
     attempts = 0
 
     while True:
+        # Take user input for guess
         try:
-            user_guess = int(input("Guess a number between 1 and 99: "))
+            user_guess = int(input("Guess a number between 1 and 10: "))
+        # If user put invalid input Show error message
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
-
+        # Increase attempts count
         attempts += 1
 
+        # If user put Too Low Value
         if user_guess < number_to_guess:
             print(f"Attempt No. {attempts}, Too low! Try again.")
+        # If user put Too High Value
         elif user_guess > number_to_guess:
             print(f"Attempt No. {attempts}, Too high! Try again.")
+        # If user put Correct Value
         else:
             print(
                 f"Congratulations! You guessed the correct number in the {attempts} attempt.")
             break
 
 
+# If user Wants to play again
 while True:
     play_game()
     again = input("Do you want to play again? (yes/no): ").strip().lower()
---
+++ growth_mindset_challenge/03_guess_number_user.py
@@ -0,0 +1,39 @@
+import random
+
+
+def computer_guess():
+    low = 1
+    high = 10
+    # Number of Attempts
+    attempts = 0
+
+    print("Think of a number between 1 and 10, and the computer will try to guess it.")
+
+    while True:
+        # Computer's guess
+        guess = random.randint(low, high)
+        attempts += 1
+        print(f"Computer's guess: {guess}")
+
+        # User feedback
+        feedback = input(
+            "Please Enter \"H\" if too high, \"L\" if too low, or \"C\" if correct: ").strip().lower()
+
+        if feedback == "l":
+            low = guess + 1
+        elif feedback == "h":
+            high = guess - 1
+        elif feedback == "c":
+            print(f"The computer guessed your number in {attempts} attempts!")
+            break
+        else:
+            print("Please enter only 'high', 'low', or 'correct'.")
+
+
+# Option to play again
+while True:
+    computer_guess()
+    again = input("Do you want to play again? (yes/no): ").strip().lower()
+    if again != 'yes':
+        print("Thanks for playing! Goodbye.")
+        break
