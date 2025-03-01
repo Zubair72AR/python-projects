5bff8fd71affe350c65bed68f2fd133594a60155
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-03-01 12:54:01 +0500

    updated practice

---
+++ Rough/mypractice.py
@@ -0,0 +1,70 @@
+import sys
+import random
+# my_string: str = "Hallo World"
+# modified_string: str = my_string.split("l")
+# print(modified_string)
+
+# message: str = " Pakistan won all the matches of champion trophy"
+
+# len_string: str = " the number of champion "
+# print(len(len_string))
+
+# a: str = "alkfjdslfkdslfkjsdlfkjsadlfkjsadl;fkjsa;kjsdhfkshfksjfhksdhfdshkldfjghkjdfpoegldkjbe;rigjldfkjger;lgj;dfj;lkdflk;gjdflkdgjdsflkjgl;dfkgjldfkgj;dlfkjg;ldfgjl;sdfkgjsd;fklgsdf;ljg;lsdfkjg;dslfkjg;dfskgl;dfsj;gldfsjg;kjdf;lgsdjf;lgksdjf;lkgjsdfl;kgfljas;lf"
+# b: str = "alkfjdslfkdslfkjsdlfkjsadlfkjsadl;fkjsa;kjsdhfkshfksjfhksdhfdshkldfjghkjdfpoegldkjbe;rigjldfkjger;lgj;dfj;lkdflk;gjdflkdgjdsflkjgl;dfkgjldfkgj;dlfkjg;ldfgjl;sdfkgjsd;fklgsdf;ljg;lsdfkjg;dslfkjg;dfskgl;dfsj;gldfsjg;kjdf;lgsdjf;lgksdjf;lkgjsdfl;kgfljas;lf"
+# c: str = "alkfjdslfkdslfkjsdlfkjsadlfkjsadl;fkjsa;kjsdhfkshfksjfhksdhfdshkldfjghkjdfpoegldkjbe;rigjldfkjger;lgj;dfj;lkdflk;gjdflkdgjdsflkjgl;dfkgjldfkgj;dlfkjg;ldfgjl;sdfkgjsd;fklgsdf;ljg;lsdfkjg;dslfkjg;dfskgl;dfsj;gldfsjg;kjdf;lgsdjf;lgksdjf;lkgjsdfl;kgfljas;lf"
+# print(len(a))
+# print(a is b is c)
+# d: str = ("a b")
+# e: str = ("a b")
+# print(d is e)
+# f: str = sys.intern("xyz")
+# g: str = sys.intern("abc")
+# print(f is g)
+
+# fruits: list = ["apple", "orange", "banana"]
+
+# print(len(fruits[0]))
+
+# fruits.pop()  # removes last element
+# fruits.append("Strawberry")  # can add one value at a time
+# fruits.pop(1)  # removes given number element
+# fruits.insert(2, "pineapple")  # add value at the desired position
+# fruits.extend(["mango", "kiwi", "dates"])  # add multiple values
+# print(fruits)
+
+# person: dict = {
+#     "name": "Sir Zia Khan",
+#     "age": 45,
+#     "isTeacher": True,
+#     "class": {
+#         "time": "2 to 5",
+#         "day": "Saturday",
+#         "subjects": my_string
+#     }
+# }
+
+# print(person["class"]["subjects"])
+
+# random_number: int = random.randint(1, 120)
+
+# if (50 > random_number < 49):
+# print("Fail: Score was", random_number)
+# elif (random_number < 99):
+# print("OK: Score was", random_number)
+# else:
+# print("King: Score was", random_number)
+
+
+# team: tuple = ("babar", "naseem", "hassan")
+# print(team)
+
+value_Set: set = {0, 1, 1, 3, 4, 5, 6, 7, 8}
+print(value_Set)
+
+fruits_set: set = {"apple", "orange", "orange", "banana"}
+print(fruits_set)
+print(type(fruits_set))
+
+# Assignment
+# revision till repo 8 modules functions
+# generative ai deep learning machine learning =presentation share it on linked
