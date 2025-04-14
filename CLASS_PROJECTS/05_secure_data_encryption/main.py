34b500e4ef6aac7dad93773ecba9948b06434a3a
Zubair72AR <168182831+Zubair72AR@users.noreply.github.com>
2025-04-14 12:59:16 +0500

    updated

--- CLASS_PROJECTS/05_secure_data_encryption/main.py
+++ CLASS_PROJECTS/05_secure_data_encryption/main.py
@@ -1,12 +1,3 @@
-"""
-Script description: This script imports tests the Streamlit-Authenticator package. 
-
-Libraries imported:
--------------------
-- yaml: Module implementing the data serialization used for human readable documents.
-- streamlit: Framework used to build pure Python web applications.
-"""
-
 import yaml
 import streamlit as st
 from yaml.loader import SafeLoader
@@ -17,9 +8,6 @@ from streamlit_authenticator.utilities import *
 with open('config.yaml', 'r', encoding='utf-8') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-# Pre-hashing all plain text passwords once
-# stauth.Hasher.hash_passwords(config['credentials'])
-
 # Creating the authenticator object
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -28,24 +16,12 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-# authenticator = stauth.Authenticate(
-#     '../config.yaml'
-# )
-
 # Creating a login widget
 try:
     authenticator.login()
 except LoginError as e:
     st.error(e)
 
-# Creating a guest login button
-try:
-    authenticator.experimental_guest_login('Login with Google', provider='google',
-                                           oauth2=config['oauth2'])
-    authenticator.experimental_guest_login('Login with Microsoft', provider='microsoft',
-                                           oauth2=config['oauth2'])
-except LoginError as e:
-    st.error(e)
 
 # Authenticating user
 if st.session_state['authentication_status']:
@@ -57,14 +33,6 @@ elif st.session_state['authentication_status'] is False:
 elif st.session_state['authentication_status'] is None:
     st.warning('Please enter your username and password')
 
-# Creating a password reset widget
-if st.session_state['authentication_status']:
-    try:
-        if authenticator.reset_password(st.session_state['username']):
-            st.success('Password modified successfully')
-    except (CredentialsError, ResetError) as e:
-        st.error(e)
-
 # Creating a new user registration widget
 try:
     (email_of_registered_user,
@@ -75,38 +43,6 @@ try:
 except RegisterError as e:
     st.error(e)
 
-# Creating a forgot password widget
-try:
-    (username_of_forgotten_password,
-        email_of_forgotten_password,
-        new_random_password) = authenticator.forgot_password()
-    if username_of_forgotten_password:
-        st.success('New password sent securely')
-        # Random password to be transferred to the user securely
-    elif not username_of_forgotten_password:
-        st.error('Username not found')
-except ForgotError as e:
-    st.error(e)
-
-# Creating a forgot username widget
-try:
-    (username_of_forgotten_username,
-        email_of_forgotten_username) = authenticator.forgot_username()
-    if username_of_forgotten_username:
-        st.success('Username sent securely')
-        # Username to be transferred to the user securely
-    elif not username_of_forgotten_username:
-        st.error('Email not found')
-except ForgotError as e:
-    st.error(e)
-
-# Creating an update user details widget
-if st.session_state['authentication_status']:
-    try:
-        if authenticator.update_user_details(st.session_state['username']):
-            st.success('Entry updated successfully')
-    except UpdateError as e:
-        st.error(e)
 
 # Saving config file
 with open('config.yaml', 'w', encoding='utf-8') as file:
